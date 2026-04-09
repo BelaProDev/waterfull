3700fdb3b68ec4dcf28fd6726b8b79da96abeeeb
Cecilia <waterfull.app@gmail.com>
2026-04-09 15:23:08 +0200

    debug

--- src/components/StepE.vue
+++ src/components/StepE.vue
@@ -94,7 +94,7 @@ export default {
   methods: {
     async goToStep(step) {
       await this.imgur()
-      this.$emit('choice', Object.values(Object.values(this.choice)[0])[27])
+      this.$emit('choice', this.choice)
       this.$emit('currentStepUp', step)
     },
     handleImages(files) {

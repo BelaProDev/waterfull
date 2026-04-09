071b27d036512a19e83a436f219f3ac0792ef30d
Cecilia <waterfull.app@gmail.com>
2026-04-09 15:38:11 +0200

    debug

--- functions/posts/posts.js
+++ functions/posts/posts.js
@@ -1,7 +1,6 @@
 import { neon } from '@netlify/neon';
 const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
 const apiKey = process.env.VITE_X_API_KEY
-const table = process.env.VITE_TABLE_MARKERS
 
 exports.handler = async (event, context) => {
     const requestKey = event.headers['x-api-key']
@@ -9,10 +8,9 @@ exports.handler = async (event, context) => {
         // get the data from the body of the request
         const data = JSON.parse(event.body)
         const Adata = Array.from(Object.entries(data.content), ([key, value]) => value)
-        console.log(Adata)
         try {
             // create document in existing collection
-            const response = await sql`INSERT INTO ${table} VALUES (${Adata[0]},${Adata[1]},${Adata[2]},${Adata[3][1]},${Adata[4]},${Adata[5]})`
+            const response = await sql`INSERT INTO markers_f4fp2sk9h45 VALUES (${Adata[0]},${Adata[1]},${Adata[2]},${Adata[3][1]},${Adata[4]},${Adata[5]})`
             return {
                 statusCode: 200,
                 body: JSON.stringify({
